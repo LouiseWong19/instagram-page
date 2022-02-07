9f1120a08904193936277df9d54b6e645927faf2
Louise Wong <lw17122@bristol.ac.uk>
2022-02-07 20:31:42 +0000

    added navbar component into the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss";
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
     <div className="App">
       <h1>It's working</h1>
+      <NavBar />
     </div>
   );
 };
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import AddIcon from "../NavBarIcon/AddIcon";
+import CompassIcon from "../NavBarIcon/CompassIcon";
+import HeartIcon from "../NavBarIcon/HeartIcon";
+import HomeIcon from "../NavBarIcon/HomeIcon";
+import InstagramWordIcon from "../NavBarIcon/InstagramWordIcon";
+import SendIcon from "../NavBarIcon/SendIcon";
+import "./NavBar.scss";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar__icons">
+      <InstagramWordIcon />
+      </div>
+      <div className="navbar__icons">
+        <HomeIcon />
+      </div>
+      <div className="navbar__icons">
+        <SendIcon />
+      </div>
+      <div className="navbar__icons">
+        <AddIcon />
+      </div>
+      <div className="navbar__icons">  
+        <CompassIcon />
+      </div>
+      <div className="navbar__icons">
+        <HeartIcon />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
