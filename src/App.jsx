0fbb6335d3c6b1c84d14e4c04942d502bab46605
Louise Wong <lw17122@bristol.ac.uk>
2022-02-08 17:42:54 +0000

    added styling for header detail component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import "./App.scss";
 import Bio from "./components/Bio/Bio";
+import HeaderDetail from "./components/HeaderDetail/HeaderDetail";
 import NavBar from "./components/NavBar/NavBar";
-import ProfilePicture from "./components/ProfilePicture/ProfilePicture";
 
 const App = () => {
   return (
     <div className="app">
       <NavBar />
       <div className="app__profile">
-        <ProfilePicture
-          picture="https://placekitten.com/90/90"
-          alt="profile pic"
-        />
+        <HeaderDetail username="prettyCat"/>
         <Bio
           name="pretty_cat"
           bio="hello"
--- src/components/HeaderDetail/HeaderDetail.jsx
+++ src/components/HeaderDetail/HeaderDetail.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import "./HeaderDetail.scss";
+import ProfilePicture from '../ProfilePicture/ProfilePicture';
 
-const HeaderDetail = () => {
-  return <div></div>;
+const HeaderDetail = (props) => {
+  const {username} = props
+  return <div className='header-detail'>
+    <ProfilePicture
+          picture="https://placekitten.com/90/90"
+          alt="profile pic"
+        />
+    <div className='header-detail__name'>
+      <h2>{username}</h2>
+    </div>
+  </div>;
 };
 
 export default HeaderDetail;
