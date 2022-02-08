e342e8d6f4fc5479a7438f0d54fe8abcc86c422c
Louise Wong <lw17122@bristol.ac.uk>
2022-02-08 16:02:25 +0000

    created bio component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
+import Bio from "./components/Bio/Bio";
 import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
+      <Bio name="pretty_cat" bio="hello" website="#"/>
     </div>
   );
 };
--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import "./Bio.scss";
 
-const Bio = () => {
-  return <div>
-
+const Bio = (props) => {
+  const {name, bio, website} = props;
+  return <div className='bio'>
+    <h3>{name}</h3>
+    <p>{bio}</p>
+    <a href={website}>{website}</a>
   </div>;
 };
 
