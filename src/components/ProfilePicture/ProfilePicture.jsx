b89cd2162a540478decc9ea58e4b5e9d0875fb61
Louise Wong <lw17122@bristol.ac.uk>
2022-02-08 17:02:32 +0000

    created profile picture component, added img tag

--- src/components/ProfilePicture/ProfilePicture.jsx
+++ src/components/ProfilePicture/ProfilePicture.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const ProfilePicture = () => {
-  return <div></div>;
+const ProfilePicture = (props) => {
+  const {picture, alt} = props
+  return <div className='profile-picture'>
+    <img src={picture} alt={alt} className="profile-picture__image" />
+  </div>;
 };
 
 export default ProfilePicture;
