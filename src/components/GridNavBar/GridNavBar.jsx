4713d02f8f0aa82dd2f517009cc2788f54e6a51c
Louise Wong <lw17122@bristol.ac.uk>
2022-02-11 13:01:33 +0000

    added icon files for grid nav bar

---
+++ src/components/GridNavBar/GridNavBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./GridNavBar.scss";
+
+const GridNavBar = () => {
+  return <div>GridNavBar</div>;
+};
+
+export default GridNavBar;
---
+++ src/components/GridNavBarIcon/VideoIcon.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const VideoIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="img"
+      width="1em"
+      height="1em"
+      preserveAspectRatio="xMidYMid meet"
+      viewBox="0 0 48 48"
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M39 6H9a3 3 0 0 0-3 3v30a3 3 0 0 0 3 3h30a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3z" />
+        <path d="M20.5 28v-6.062l5.25 3.03L31 28l-5.25 3.031l-5.25 3.031V28z" />
+        <path d="M6 15h36" />
+        <path d="M33 6l-6 9" />
+        <path d="M21 6l-6 9" />
+      </g>
+    </svg>
+  );
+};
+
+export default VideoIcon;
